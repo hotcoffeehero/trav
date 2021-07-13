74473c8904dce53c9aa571e890ae65c90ee0f475
hotcoffeehero <hotcoffeehero@gmail.com>
2021-07-13 20:46:21 +0900

    Drink Water 1

---
+++ 5050/16_drink_water/script.js
@@ -0,0 +1,25 @@
+const smallCups = document.querySelectorAll('cup-small')
+const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener('click', () => highlightCups(index))
+})
+
+function highlightCups(index) {
+  if (
+    smallCups[index].classList.contains('full') &&
+    !smallCups[index].nextElementSibling.classList.contains('full')
+  ) {
+    index--
+  }
+
+  smallCups.forEach((cup, indx2) => {
+    if (indx2 <= index) {
+      cup.classList.add('full')
+    } else {
+      cup.classList.remove('full')
+    }
+  })
+}
