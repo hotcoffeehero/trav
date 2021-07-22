0ea111da1897ce72fa8b0282ae0216d24aa75a6f
hotcoffeehero <hotcoffeehero@gmail.com>
2021-07-22 17:46:06 +0900

    Drink Water

--- 5050/16_drink_water/script.js
+++ 5050/16_drink_water/script.js
@@ -1,7 +1,9 @@
-const smallCups = document.querySelectorAll('cup-small')
+const smallCups = document.querySelectorAll('.cup-small')
 const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
+
+updateBigCup()
 
 smallCups.forEach((cup, index) => {
   cup.addEventListener('click', () => highlightCups(index))
@@ -22,4 +24,32 @@ function highlightCups(index) {
       cup.classList.remove('full')
     }
   })
+
+  updateBigCup()
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+  const totalCups = smallCups.length
+  const finMessage = document.querySelector('.fin-message')
+
+  console.log(totalCups)
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden'
+    percentage.style.height = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden'
+    remained.style.height = 0
+    finMessage.style.display = 'block'
+  } else {
+    remained.style.visibility = 'visible'
+    liters.innerText = `${2 - (250 * fullCups) / 1000}`
+  }
 }
