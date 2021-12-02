7c0c82b8fa5e097aa9cde18aba5f6f1f978e0b24
hotcoffeehero <hotcoffeehero@gmail.com>
2021-12-02 15:42:19 +0900

    11: User Registration

--- devconnector/routes/api/users.js
+++ devconnector/routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar')
 const { check, validationResult } = require('express-validator')
+const User = require('../../models/User')
+const bcrypt = require('bcryptjs')
+
 
 // @route   POST api/users
 // @desc    test route
@@ -9,12 +13,49 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Enter a valid email').isEmail(),
     check('password', 'Enter a valid password at least six characters long').isLength({min: 6})
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()})
     }
-    res.send('User Route')
+    
+    const { name, email, password } = req.body
+    try {
+        //See if the user exists
+        let user = await User.findOne({ email })
+
+        if(user){
+           return res.status(400).json({errors: [{msg: 'User Already Exists'}]})
+        }
+
+        //Get user's gravatar
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name, 
+            email,
+            avatar,
+            password
+        })
+
+        //Encrypt password
+        const salt = await bcrypt.genSalt(10)
+
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save()
+
+        //Return jswebtoken
+        res.send('New User Registered')
+    } catch (err) {
+     console.error(err)   
+     res.status(500).send('Server Error')
+    }
+    
 })
 
 module.exports = router
