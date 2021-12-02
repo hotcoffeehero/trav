ecdc763c309137342368e26037fb8a246fb22191
hotcoffeehero <hotcoffeehero@gmail.com>
2021-12-02 09:17:19 +0900

    10: Request & Body Validation

--- devconnector/routes/api/users.js
+++ devconnector/routes/api/users.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
-// @route   GET api/users
+// @route   POST api/users
 // @desc    test route
 // @access  Public
-router.get('/', (req, res) => res.send('User Route'))
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Enter a valid email').isEmail(),
+    check('password', 'Enter a valid password at least six characters long').isLength({min: 6})
+], (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+    res.send('User Route')
+})
 
 module.exports = router
