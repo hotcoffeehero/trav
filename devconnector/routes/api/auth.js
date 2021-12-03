d14762f6eed1e6b9bcb48f6da00d289e9f52f61c
hotcoffeehero <hotcoffeehero@gmail.com>
2021-12-03 17:35:58 +0900

    14: User Authentication & Login Route

--- devconnector/routes/api/auth.js
+++ devconnector/routes/api/auth.js
@@ -3,6 +3,11 @@ const router = express.Router()
 const auth = require('../../middleware/auth')
 const User = require('../../models/User')
 
+const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
 // @route   GET api/auth
 // @desc    test route
 // @access  Public
@@ -15,7 +20,60 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('There was a server error')
     }
 }
-
 )
+
+// @route   POST api/auth
+// @desc    Authenticate User & Get token 
+// @access  Public
+router.post('/', [
+    check('email', 'Enter your email').isEmail(),
+    check('password', 'Enter your password').exists()
+], async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+    
+    const { email, password } = req.body
+
+    try {
+        //See if the user exists
+        let user = await User.findOne({ email })
+
+        if(!user){
+           return res.status(400).json({errors: [{msg: 'Invalid Credentials'}]})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch) {
+            return res.status(400).json({errors: [{msg: 'Invalid Credentials'}]})
+
+        }
+
+        //Return jswebtoken
+        const payload = {
+            user: {
+                id: user._id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtToken'),
+            {expiresIn: 3600000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({token})
+            }
+            )
+
+    } catch (err) {
+     console.error(err)   
+     res.status(500).send('Server Error')
+    }
+    
+})
+
 
 module.exports = router
--- devconnector/routes/api/users.js
+++ devconnector/routes/api/users.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
-const { check, validationResult } = require('express-validator')
 const User = require('../../models/User')
 const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken')
 const config = require('config')
 
