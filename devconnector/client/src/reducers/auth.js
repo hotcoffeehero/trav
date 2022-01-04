05a8b99eb14f0d87cab3e34e8c6a28e32fcd31fa
hotcoffeehero <hotcoffeehero@gmail.com>
2022-01-05 01:17:16 +0900

    Logout

--- devconnector/client/src/reducers/auth.js
+++ devconnector/client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -13,10 +14,8 @@ const initialState = {
   loading: true,
   user: null
 };
-
 export default function(state = initialState, action) {
   const { type, payload } = action;
-
   switch (type) {
     case USER_LOADED:
       return {
@@ -37,7 +36,7 @@ export default function(state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
-      console.log(123);
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
--- devconnector/routes/api/auth.js
+++ devconnector/routes/api/auth.js
@@ -9,8 +9,8 @@ const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 
 // @route    GET api/auth
-// @desc     Test route
-// @access   Public
+// @desc     Get user by token
+// @access   Private
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
@@ -26,10 +26,8 @@ router.get('/', auth, async (req, res) => {
 // @access   Public
 router.post(
   '/',
-  [
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Password is required').exists()
-  ],
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password is required').exists(),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -64,7 +62,7 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 360000 },
+        { expiresIn: '5 days' },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
