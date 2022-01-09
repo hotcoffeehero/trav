e0739e8ddfe6323d1dd2bc674e5ae2a03ce69da1
hotcoffeehero <hotcoffeehero@gmail.com>
2022-01-09 14:18:49 +0900

    51:List Education and Experiences

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -7,7 +7,10 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -41,6 +44,21 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
               />
             </Switch>
           </section>
